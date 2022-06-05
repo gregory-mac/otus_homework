9c98f4877d7c4b0aaadb74e99aa4337a79f6463d
john <john@example.com>
2022-06-05 20:28:17 +0300

    Minor changes

--- homework_05/app.py
+++ homework_05/app.py
@@ -13,13 +13,11 @@
 
 from flask import (
     Flask,
-    request,
     render_template
 )
 
 
 app = Flask(__name__)
-app.config.update(ENV="development", SECRET_KEY="verysecretkey")
 
 
 @app.route("/", endpoint="index")
@@ -33,4 +31,4 @@ def about_page():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(port=5000)
