ebbb197923ef406a90133115bd3b5da77cd2ea92
john <john@example.com>
2022-06-28 10:22:15 +0300

    Homework 06 initial

---
+++ homework_06/blog-app/models/post.py
@@ -0,0 +1,17 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    ForeignKey
+)
+from sqlalchemy.orm import relationship
+
+from .database import db
+
+
+class Post(db.Model):
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False, unique=False)
+    title = Column(String(100), nullable=False, unique=True)
+    body = Column(String(500), nullable=False, unique=False)
+
+    user = relationship("User", back_populates="posts")
