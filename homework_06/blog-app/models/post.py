0f992c1cd1cb9875298710e1464d650778f5c438
john <john@example.com>
2022-07-02 15:43:31 +0300

    Root is working

--- homework_06/blog-app/models/post.py
+++ homework_06/blog-app/models/post.py
@@ -10,8 +10,10 @@ from .database import db
 
 
 class Post(db.Model):
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False, unique=False)
-    title = Column(String(100), nullable=False, unique=True, primary_key=True)
+    title = Column(String(100), nullable=False, unique=True)
     body = Column(String(500), nullable=False, unique=False)
 
     user = relationship("User", back_populates="posts")
